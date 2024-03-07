2818ef16af48a078dfb8adcfd12cf0576a91d08d
Caio Cavalcante <caio.melo51@gmail.com>
2024-03-07 11:36:33 -0300

    Making smarter routes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/perfil")
-def perfil():
-    return render_template("perfil.html")
+@app.route("/perfil/<usuario>")
+def perfil(usuario):
+    return render_template("perfil.html", usuario = usuario)
 
 if __name__ == "__main__":
     app.run(debug=True)
