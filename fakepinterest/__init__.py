bb34d7a93d605273c3e6b12239c49b63d36b72da
Caio Cavalcante <caio.melo51@gmail.com>
2024-03-07 13:07:43 -0300

    Starting login implementation

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
-
+app.config["SECRET_KEY"] = "3a3d19b1df7ec78f7ae02b8c36db2d5a"
 
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
 
 from fakepinterest import routes
--- fakepinterest/models.py
+++ fakepinterest/models.py
@@ -1,7 +1,15 @@
 #Criar a estrutura do banco de dados
-from fakepinterest import database
+from fakepinterest import database, login_manager
 from datetime import datetime
-class User(database.Model):
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(id_user):
+    return User.query.get(int(id_user))
+
+
+class User(database.Model, UserMixin):
     id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String, nullable=False)
     email = database.Column(database.String, unique=True)
