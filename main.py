e37070f592c05d2db930cadf5311f9504b20379a
Caio Cavalcante <caio.melo51@gmail.com>
2024-03-07 11:24:11 -0300

    Adding HTML Archives

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "FakePinterest - Meu primeiro site no ar"
+    return render_template("homepage.html")
+
+@app.route("/perfil")
+def perfil():
+    return render_template("perfil.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
