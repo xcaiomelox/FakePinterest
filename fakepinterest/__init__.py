3776ab73e0e0acf62b7a40843da82be56d6d53e0
Caio Cavalcante <caio.melo51@gmail.com>
2024-03-10 15:33:41 -0300

    criando gerenciamento de imagens

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -6,6 +6,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "3a3d19b1df7ec78f7ae02b8c36db2d5a"
+app.config["UPLOAD_FOLDER"] = "static/photo_posts"
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
